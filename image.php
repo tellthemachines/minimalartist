b144f47c197de5bdf370c3727c5ecf9a75e236bc
tellthemachines <isabel@tellthemachines.com>
2015-05-05 12:07:09 +1000

    styling improvements

--- image.php
+++ image.php
@@ -10,17 +10,17 @@ get_header();
 	<?php
 		// Start the Loop.
 		while ( have_posts() ) : the_post();
-	
+
 			$postid = get_posts( array(
             					'post_type' => 'attachment',
 					            'posts_per_page' => -1,
 					            'post_parent' => $post->ID
 							) ); ?>
-			
+
 			<article class="imagebox">
 				<div class="attachbox">
-			 	<a href="<?php echo wp_get_attachment_url($postid); ?>"> 
-				<?php  echo wp_get_attachment_image($postid, 'medium');  ?>  </a> 
+			 	<a href="<?php echo wp_get_attachment_url($postid); ?>">
+				<?php  echo wp_get_attachment_image($postid, 'large');  ?>  </a>
 				</div><!-- .attachment -->
 
 				<?php  if ( has_excerpt()) : ?>
@@ -29,11 +29,11 @@ get_header();
 				</div><!-- .entry-caption -->
 				<?php endif; ?>
 			</article> <!-- .imagebox -->
-	
-			<nav id="imagenav" class="navs" role="navigation" aria-label="<?php _e( 'Image gallery navigation', 'minart' ); ?>">	
+
+			<nav id="imagenav" class="navs" role="navigation" aria-label="<?php _e( 'Image gallery navigation', 'minart' ); ?>">
 				<span class="previous-image"><?php previous_image_link( false,  __( 'Previous', 'minart' )); ?></span>
 				<span class="parent-post-link"><a href="<?php echo get_permalink( $post->post_parent ); ?>" rel="gallery"><?php _e('exit gallery','minart'); ?></a></span>
-				<span class="next-image"><?php next_image_link( false,  __( 'Next', 'minart' )); ?></span>	
+				<span class="next-image"><?php next_image_link( false,  __( 'Next', 'minart' )); ?></span>
 			</nav><!-- #image-navigation -->
 
 		<?php endwhile; // end of the loop. ?>
@@ -42,4 +42,3 @@ get_header();
 
 <?php
 get_footer();
-
